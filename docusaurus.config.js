2083626e3aa1147656f22320f05528c6e4e41fdb
Guangbo Chen <steven.guangbo.chen@gmail.com>
2023-11-29 17:42:05 +0800

    Add plugins and CNAME

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -129,6 +129,25 @@ const config = {
         darkTheme: prismThemes.dracula,
       },
     }),
+  plugins: [
+    [
+      '@docusaurus/plugin-ideal-image',
+      {
+        quality: 70,
+        max: 1030, // max resized image's size.
+        min: 640, // min resized image's size. if original is lower, use that size.
+        steps: 2, // the max number of images generated between min and max (inclusive)
+        disableInDev: false,
+      },
+    ],
+    [
+      '@docusaurus/plugin-google-gtag',
+      {
+        trackingID: 'GTM-KK97PBTN',
+        anonymizeIP: true,
+      },
+    ],
+  ],
 };
 
 export default config;
